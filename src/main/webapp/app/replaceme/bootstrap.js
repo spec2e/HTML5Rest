6ed5749a27ab66305fbc84e9ecfb913537630ff6
Soeren Pedersen <speconsult@gmail.com>
2013-05-09 16:38:08 +0200

    fix

--- src/main/webapp/app/replaceme/bootstrap.js
+++ src/main/webapp/app/replaceme/bootstrap.js
@@ -65,3 +65,17 @@ replacemeModule.factory('todoService', ['$resource', '$routeParams',
 ]);
 
 
+//ngBlur directive
+
+replacemeModule.directive('ngBlur', ['$parse', function($parse) {
+    return function(scope, element, attr) {
+        var fn = $parse(attr['ngBlur']);
+        element.bind('blur', function(event) {
+            scope.$apply(function() {
+                fn(scope, {$event:event});
+            });
+        });
+    }
+}]);
+
+
