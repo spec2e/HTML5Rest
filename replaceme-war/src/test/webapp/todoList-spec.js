f189f3cb2e41a343599bc5c1f82b735e0c114998
Soeren Pedersen <speconsult@gmail.com>
2013-05-11 10:40:18 +0200

    e2e

--- replaceme-war/src/test/webapp/todoList-spec.js
+++ replaceme-war/src/test/webapp/todoList-spec.js
@@ -7,7 +7,7 @@ describe('replaceme', function() {
 
 
     it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-        expect(browser().location().url()).toBe('/home');
+        expect(browser().location().url()).toEqual('/home');
     });
 
 
@@ -20,6 +20,20 @@ describe('replaceme', function() {
 
         it('should write welcome message on the first page', function() {
             expect(element('h3').text()).toBe('Welcome to the HTML5/REST demo app :-)')
+        });
+
+    });
+
+    describe('Goto list page', function() {
+
+        beforeEach(function() {
+            browser().navigateTo('#/home');
+        });
+
+
+        it('should go to the list page, when the list link is clicked', function() {
+            expect(element('#list-link').click());
+            expect(browser().location().url()).toEqual('/todo/list');
         });
 
     });
