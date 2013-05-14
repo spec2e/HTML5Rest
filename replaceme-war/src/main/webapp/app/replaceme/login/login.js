0b8256164d1c560cc2e547a0dc0fc6dc3aa13a31
soeren.pedersen <svx@topdanmark.dk>
2013-05-14 16:25:05 +0200

    login

--- replaceme-war/src/main/webapp/app/replaceme/login/login.js
+++ replaceme-war/src/main/webapp/app/replaceme/login/login.js
@@ -1,7 +1,25 @@
 namespace('replaceme.login');
 
-replaceme.login.LoginController = function($scope, $routeParams, $resource, todoService) {
+replaceme.login.LoginController = function($scope, authHttp, todoService) {
 	
+	$scope.userName;
+	$scope.password;
 	
+	$scope.login = function() {
+		
+		var postData = {
+				userName: $scope.userName,
+				password: $scope.password
+		};
+		
+		authHttp.post('rest/Login/login/' + $scope.userName + "/" + $scope.password, postData).
+			success(function(data, status, headers, config) {
+				console.log('success: status is: ' + status);
+				
+			}).error(function(data, status, headers, config) {
+				console.log('error: status is: ' + status);
+		});
+		
+	};
 	
 };
